a156354cc083404710e85fba624f7307a44fe91c
Roman <s91630277@gmail.com>
2014-04-17 03:12:30 +0400

    Added all-timers.js

---
+++ differences/all-timers.js
@@ -0,0 +1,60 @@
+var fs = require("fs");
+
+console.log("Begin");
+
+fs.open(__filename, "r", function(err, file) {
+	console.log("I/O");
+});
+
+setImmediate(function() {
+	console.log("Immediate should be after I/O events");
+});
+
+process.nextTick(function() {
+	console.log("nextTick");
+});
+
+// setTimeout(function() {
+// 	console.log("Timeout");
+// }, 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// WIN
+// nextTick
+// Immediate after I/O events
+// I/O
+// ON LINUX
+// Begin
+// nextTick
+// Timeout
+// I/O
+// Immediate after I/O events
