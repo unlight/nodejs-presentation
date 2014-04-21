e9db7636540706263e627dd7964c70ccfdd7ebd2
Roman <s91630277@gmail.com>
2014-04-22 03:18:41 +0400

    App

---
+++ module-example/sum.js
@@ -0,0 +1,11 @@
+// sum.js
+function sum() {
+	var args = [].slice.call(arguments);
+	var result = 0;
+	args.forEach(function(value) {
+		result += value;
+	});
+	return result;
+}
+
+module.exports = sum;
