af1f07d096972de0214feafbe15f5c794ea56f1f
Roman <s91630277@gmail.com>
2014-04-29 20:51:05 +0400

    A

--- server-example/server-example-1.js
+++ server-example/server-example-1.js
@@ -1,7 +1,9 @@
 var http = require('http'); // How we require modules 
 var server = http.createServer(function(request, response) { 
     response.writeHead(200); // Status code in header 
-    response.write("You requested url: " + request.url + ", method: " + request.method); // Response body 
+     // Response body 
+    response.write(" You requested url: " + request.url);
+    response.write(" You requested method: " + request.method);
     response.end(); // Close the connection 
 });
 server.listen(8080); // Listen for connections on this port
