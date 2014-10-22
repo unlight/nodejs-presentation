feddbe1de5b8ff2a723de2e45e0f3780e21d9a72
Roman <s91630277@gmail.com>
2014-10-22 23:06:30 +0400

    Moved some files

--- events-and-streams/why-you-should-use.js
+++ events-and-streams/why-you-should-use.js
@@ -1,11 +1,14 @@
-var http = require('http');
-var fs = require('fs');
+var http = require("http");
+var fs = require("fs");
 
 var server = http.createServer(function (req, res) {
-    fs.readFile(__dirname + '/data.txt', function (err, data) {
+    fs.readFile(__dirname + "/data.txt", function (err, data) {
+    	if (err) throw err;
     	res.write(data);
         res.end();
     });
 });
-server.listen(8080);
 
+server.listen(8080, function() {
+	console.log("Created server http://localhost:8080");
+});
